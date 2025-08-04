3e3c07b293cc10d0898a1f78897d4762c9b563fa
arthurm41 <artcromaia9@gmail.com>
2025-08-04 16:58:04 -0300

    04/08/25

---
+++ aula 01/variaveis.php
@@ -0,0 +1,31 @@
+<?php
+/*Desenvolva um codigo com a mesma estrutura que o abaixo,porem com os seus dados.*/
+
+    echo"Olá! \n";
+    $nome ="Arthur! \n";
+    $idade ="17";
+    $ano_atual = "2025";
+
+    $data_nasc= $ano_atual-$idade;
+    echo $nome, "você nasceu em:", $data_nasc;
+
+
+// Faça as questões 2 e 3 do slide Aula 01 - Fundamentos basicos
+
+$str = "Programação em php";
+echo "\nMinúsculo,";
+$str = strtoupper(string: $str);
+echo "\Maiúsculo: ", $str;
+$str = strtolower(string: $str);
+echo "\nMinusculo novamente: ", $str;
+
+
+// exercicio 3
+
+$exerc3 = "o php foi criado em mil novecentos e noventa e cinco";
+echo "\nAntes do comando replace: \n",$exerc3;
+$exerc3 =  str_replace(['o','a','i']['0','4','1'], subject; $exerc3);
+echo "\nApós o comando replace: \n", $exerc3
+
+   ?>
+
